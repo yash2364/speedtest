6ffd952e078659cbf66627cf1820e9788118d413
yash2364 <yashng23648@gmail.com>
2024-06-15 22:40:42 +0530

    Update getIP_ipInfo_apikey.php

--- backend/getIP_ipInfo_apikey.php
+++ backend/getIP_ipInfo_apikey.php
@@ -1,4 +1,26 @@
-<?php
+/**
+ * @param string $ip
+ *
+ * @return array|null
+ */
+function getIspInfo($ip)
+{
+    global $IPINFO_APIKEY;
 
-// put your token between the quotes if you have one
-$IPINFO_APIKEY = '';
+    // Check if API key is defined and not empty
+    if (!isset($IPINFO_APIKEY) || empty($IPINFO_APIKEY)) {
+        return null; // or handle the case where API key is missing
+    }
+
+    $json = file_get_contents('https://ipinfo.io/' . $ip . '/json' . getIpInfoTokenString());
+    if (!is_string($json)) {
+        return null;
+    }
+
+    $data = json_decode($json, true);
+    if (!is_array($data)) {
+        return null;
+    }
+
+    return $data;
+}
